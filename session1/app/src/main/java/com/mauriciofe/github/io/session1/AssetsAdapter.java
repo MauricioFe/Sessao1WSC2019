93a1c9cf00f6a95f6a61f2de21af8f010475262a
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-02-04 11:36:45 -0300

    Consumindo api para preencher a listview e os spinners

--- session1/app/src/main/java/com/mauriciofe/github/io/session1/AssetsAdapter.java
+++ session1/app/src/main/java/com/mauriciofe/github/io/session1/AssetsAdapter.java
@@ -1,27 +1,58 @@
 package com.mauriciofe.github.io.session1;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mauriciofe.github.io.session1.models.Assets;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AssetsAdapter extends BaseAdapter {
+    private List<Assets> assetsList = new ArrayList<>();
+    private Context context;
+
+    public AssetsAdapter(List<Assets> assetsList, Context context) {
+        this.assetsList = assetsList;
+        this.context = context;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return assetsList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return assetsList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return assetsList.get(position).getId();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.assets_list_item, parent, false);
+        }
+        TextView txtAssetName = convertView.findViewById(R.id.txtAssetName);
+        TextView txtAssetSN = convertView.findViewById(R.id.txtAssetSN);
+        TextView txtDepartment = convertView.findViewById(R.id.txtDepartment);
+        ImageView imgAssetImage = convertView.findViewById(R.id.imgAssetImage);
+        ImageView imgEdit = convertView.findViewById(R.id.imgEdit);
+        ImageView imgTransfer = convertView.findViewById(R.id.imgTransfer);
+        ImageView imgHistory = convertView.findViewById(R.id.imgHistory);
+        Assets assets = (Assets) getItem(position);
+        txtAssetName.setText(assets.getAssetName());
+        txtAssetSN.setText(assets.getAssetSn());
+        txtDepartment.setText(assets.getNameDescription());
+        return convertView;
     }
 }
--- session1/app/src/main/java/com/mauriciofe/github/io/session1/MainActivity.java
+++ session1/app/src/main/java/com/mauriciofe/github/io/session1/MainActivity.java
@@ -1,20 +1,31 @@
 package com.mauriciofe.github.io.session1;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mauriciofe.github.io.session1.models.AssetGroup;
+import com.mauriciofe.github.io.session1.models.Assets;
+import com.mauriciofe.github.io.session1.models.Department;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Spinner;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,19 +35,26 @@ public class MainActivity extends AppCompatActivity {
     EditText edtEndDate;
     EditText edtSearch;
     ListView listViewAssets;
+    List<AssetGroup> assetGroupList;
+    List<Department> departmentsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        spnDepartment = findViewById(R.id.main_spn_department);
-        spnAssetGroup = findViewById(R.id.main_spn_asset_group);
-        edtStartDate = findViewById(R.id.editEndDate);
-        edtEndDate = findViewById(R.id.editStartDate);
-        edtSearch = findViewById(R.id.editTextSearch);
-        listViewAssets = findViewById(R.id.listViewAssets);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        inicializandoViews();
+        assetGroupList = new ArrayList<>();
+        departmentsList = new ArrayList<>();
+        AssetsTask task = new AssetsTask();
+        AssetGroupTask taskGroup = new AssetGroupTask();
+        DepartmentTask taskDepartment = new DepartmentTask();
+
+        taskGroup.execute("http://192.168.0.101:5000/api/AssetGroups");
+        taskDepartment.execute("http://192.168.0.101:5000/api/AssetGroups");
+        task.execute("http://192.168.0.101:5000/api/assets");
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +63,149 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void inicializandoViews() {
+        spnDepartment = findViewById(R.id.main_spn_department);
+        spnAssetGroup = findViewById(R.id.main_spn_asset_group);
+        edtStartDate = findViewById(R.id.editEndDate);
+        edtEndDate = findViewById(R.id.editStartDate);
+        edtSearch = findViewById(R.id.editTextSearch);
+        listViewAssets = findViewById(R.id.listViewAssets);
+    }
+
+    public class AssetsTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            BufferedReader reader;
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("content-type", "application/json");
+                conn.setDoOutput(false);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                return stringBuilder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String content) {
+            List<Assets> assetsList = new ArrayList<>();
+            try {
+                JSONArray jsonArray = new JSONArray(content);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    Assets assets = new Assets();
+                    assets.setId(jsonObject.getInt("id"));
+                    assets.setAssetName(jsonObject.getString("assetName"));
+                    assets.setAssetSn(jsonObject.getString("assetSn"));
+                    assets.setNameDescription(jsonObject.getString("nameDescription"));
+                    assetsList.add(assets);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            AssetsAdapter adapter = new AssetsAdapter(assetsList, MainActivity.this);
+            listViewAssets.setAdapter(adapter);
+        }
+    }
+
+    public class AssetGroupTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            BufferedReader reader;
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("content-type", "application/json");
+                conn.setDoOutput(false);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                return stringBuilder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String content) {
+            List<AssetGroup> assetGroupsList = new ArrayList<>();
+            try {
+                JSONArray jsonArray = new JSONArray(content);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    AssetGroup assetGroup = new AssetGroup();
+                    assetGroup.setId(jsonObject.getInt("id"));
+                    assetGroup.setName(jsonObject.getString("name"));
+                    assetGroupsList.add(assetGroup);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            assetGroupList = assetGroupsList;
+        }
+    }
+    public class DepartmentTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            BufferedReader reader;
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("content-type", "application/json");
+                conn.setDoOutput(false);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                return stringBuilder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String content) {
+            List<Department> departmentList = new ArrayList<>();
+            try {
+                JSONArray jsonArray = new JSONArray(content);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    Department department = new Department();
+                    department.setId(jsonObject.getInt("id"));
+                    department.setName(jsonObject.getString("name"));
+                    departmentList.add(department);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            departmentsList = departmentList;
+        }
     }
 }
