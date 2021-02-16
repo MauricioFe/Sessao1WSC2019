7b8b94050febeb082f4051810b81ecf55630be60
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-02-16 14:05:45 -0300

    Criando tela para histórico de transferencia

---
+++ session1/app/src/main/java/com/mauriciofe/github/io/session1/TransferHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.mauriciofe.github.io.session1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransferHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer_history);
+    }
+}
