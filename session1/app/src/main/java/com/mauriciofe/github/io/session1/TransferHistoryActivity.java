8ed5c9d24603d62fae35d7729c124c65602a65cf
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-02-16 14:29:03 -0300

    Criação do adapter e do layout para a lista

--- session1/app/src/main/java/com/mauriciofe/github/io/session1/TransferHistoryActivity.java
+++ session1/app/src/main/java/com/mauriciofe/github/io/session1/TransferHistoryActivity.java
@@ -1,14 +1,20 @@
 package com.mauriciofe.github.io.session1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TransferHistoryActivity extends AppCompatActivity {
 
+    RecyclerView listHistoryAsset;
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer_history);
+        btnBack = findViewById(R.id.btnBack);
+        listHistoryAsset = findViewById(R.id.list_history_transfer);
     }
 }
