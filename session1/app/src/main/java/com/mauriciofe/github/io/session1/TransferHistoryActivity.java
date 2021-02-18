565e588f593f566419e890fd5fce27239bb35915
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-02-18 14:31:53 -0300

    Finalização da sessão

--- session1/app/src/main/java/com/mauriciofe/github/io/session1/TransferHistoryActivity.java
+++ session1/app/src/main/java/com/mauriciofe/github/io/session1/TransferHistoryActivity.java
@@ -1,22 +1,26 @@
 package com.mauriciofe.github.io.session1;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.mauriciofe.github.io.session1.models.AssetTransferLogs;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONStringer;
 
-import java.security.DomainLoadStoreParameter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +30,7 @@ public class TransferHistoryActivity extends AppCompatActivity {
 
     RecyclerView listHistoryAsset;
     Button btnBack;
+    TextView txtMensagem;
     List<AssetTransferLogs> logsList = new ArrayList<>();
     TransferHistoryAdapter adapter;
     long assetId;
@@ -33,8 +38,10 @@ public class TransferHistoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("History of transfer");
         setContentView(R.layout.activity_transfer_history);
         btnBack = findViewById(R.id.btnBack);
+        txtMensagem = findViewById(R.id.txtMensagem);
         listHistoryAsset = findViewById(R.id.list_history_transfer);
         adapter = new TransferHistoryAdapter(this, logsList);
         listHistoryAsset.setLayoutManager(new LinearLayoutManager(this));
@@ -48,21 +55,37 @@ public class TransferHistoryActivity extends AppCompatActivity {
             }
         });
         MyAsyncTask.requestApi(BASE_URL + "assetTransferLogs/" + assetId, MyAsyncTask.METHOD_GET, null, new Callback<String>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onComplete(String result) {
                 try {
                     JSONArray jsonArray = new JSONArray(result);
                     for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject jsonObject = jsonArray.getJSONObject(i);
-                        AssetTransferLogs log = new AssetTransferLogs(jsonObject.getInt("id"),
-                                jsonObject.getInt("assetId"), jsonObject.getString("fromAssetSN"),
-                                jsonObject.getString("fromDepartment"), jsonObject.getString("toAssetSN"),
-                                jsonObject.getString("toDepartment"), jsonObject.getInt("fromDepartmentLocationId"),
-                                jsonObject.getInt("toDepartmentLocationId"),
-                                jsonObject.getString("transferDate"));
-                        logsList.add(log);
+                        String transferDate = jsonObject.getString("transferDate").substring(0, 10);
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                        LocalDate dateTime = LocalDate.parse(transferDate, formatter);
+                        LocalDate dataTeste2 = LocalDate.now();
+                        long meses = dateTime.until(dataTeste2, ChronoUnit.MONTHS);
+                        if (meses > 12) {
+                            logsList.clear();
+                        } else {
+                            AssetTransferLogs log = new AssetTransferLogs(jsonObject.getInt("id"),
+                                    jsonObject.getInt("assetId"), jsonObject.getString("fromAssetSN"),
+                                    jsonObject.getString("fromDepartment"), jsonObject.getString("toAssetSN"),
+                                    jsonObject.getString("toDepartment"), jsonObject.getInt("fromDepartmentLocationId"),
+                                    jsonObject.getInt("toDepartmentLocationId"),
+                                    jsonObject.getString("transferDate"));
+                            logsList.add(log);
+                        }
                     }
                     adapter.atualizaList(logsList);
+                    if (logsList.size() <= 0){
+                        listHistoryAsset.setVisibility(View.INVISIBLE);
+                        txtMensagem.setVisibility(View.VISIBLE);
+                    }else{
+                        listHistoryAsset.setVisibility(View.VISIBLE);
+                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
